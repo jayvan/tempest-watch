edece50449d5f395d28e48f09a9b3d886773b5e4
Justin Vanderheide <justin@justinvanderheide.com>
2015-07-07 08:30:52 -0400

    nest /api/v0/tempests spec & make less brittle

--- spec/tempest_watch_spec.rb
+++ spec/tempest_watch_spec.rb
@@ -16,15 +16,34 @@ describe "TempestWatch" do
   end
 
   describe "API TempestsController" do
-    it "should respond to GET /" do
-      get '/api/v0/tempests'
-      expect(last_response).to be_ok
-    end
 
-    it "should return the list of tempests" do
-      get '/api/v0/tempests'
-      parsed_body = JSON.parse(last_response.body)
-      expect(parsed_body['bases']['obscuring']['name']).to be == 'Obscuring Tempest' # Quick and dirty test, brittle if data changes
+    describe "GET /api/v0/tempests" do
+      before(:all) do
+        get '/api/v0/tempests'
+        @parsed_body = JSON.parse(last_response.body)
+      end
+
+      it "should respond to GET /" do
+        expect(last_response).to be_ok
+      end
+
+      it "should return the list of base tempests" do
+        expect(@parsed_body['bases'].empty?).to be(false)
+      end
+
+      it "should have a name for each tempest" do
+        @parsed_body['bases'].each do |name, tempest|
+          expect(tempest['name']).to_not be_nil
+        end
+      end
+
+      it "should return the list of tempest prefixes" do
+        expect(@parsed_body['prefixes'].empty?).to be(false)
+      end
+
+      it "should return the list of tempest suffixes" do
+        expect(@parsed_body['suffixes'].empty?).to be(false)
+      end
     end
   end
 end
